3fc70f0690afbaf0314c63290b8d9f4b9ef8f24c
Felix Engelmann <felix.engelmann@maxiv.lu.se>
2024-01-25 23:52:31 +0100

    pileup functionality

--- src/reducer.py
+++ src/reducer.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 
 from dranspose.event import ResultData
 from dranspose.parameters import StrParameter, BoolParameter
@@ -10,11 +11,41 @@ logger = logging.getLogger(__name__)
 
 class TomoReducer:
 
+    @staticmethod
+    def describe_parameters():
+        params = [
+            BoolParameter(name="pileup"),
+        ]
+        return params
+
     def __init__(self, parameters=None, **kwargs):
-        pass
+        self.pile = None
+        self.nimages = 0
+        self.filename = None
 
     def process_result(self, result: ResultData, parameters=None):
-        pass
+        if result.payload:
+            if "filename" in result.payload:
+                fn = result.payload["filename"]
+                parts = fn.split(".")
+                self.filename = f"{'.'.join(parts[:-1])}_mean.{parts[-1]}"
+                logger.info("write to %s", self.filename)
+            if "pile" in result.payload:
+                if self.pile is None:
+                    self.pile = result.payload["pile"]
+                else:
+                    self.pile += result.payload["pile"]
+                self.nimages += result.payload["nimages"]
+
 
     def finish(self, parameters=None):
-        pass
+        if self.pile is not None:
+            Path(self.filename).parent.mkdir(parents=True, exist_ok=True)
+            if os.path.isfile(self.filename):
+                logger.error("file exists already, not saving")
+                return
+            logger.info("using pileup file %s", Path(self.filename))
+            with h5py.File(self.filename, 'w') as fh:
+                group = fh.create_group("pileup")
+                group.create_dataset("nimages", data=self.nimages)
+                group.create_dataset("data", data=(self.pile/self.nimages).astype(np.float32))
--- src/worker.py
+++ src/worker.py
@@ -5,7 +5,9 @@ import json
 from dranspose.event import EventData
 from dranspose.parameters import IntParameter, StrParameter
 from dranspose.middlewares.stream1 import parse
-from dranspose.data.stream1 import Stream1Data
+from dranspose.data.stream1 import Stream1Data, Stream1End, Stream1Start
+
+from dranspose.middlewares.sardana import parse as sardana_parse
 import numpy as np
 from numpy import unravel_index
 
@@ -17,16 +19,39 @@ class TomoWorker:
     @staticmethod
     def describe_parameters():
         params = [
-            IntParameter(name="test", default=0),
         ]
         return params
 
     def __init__(self, parameters=None, context=None, **kwargs):
-        pass
+        self.pile = None
+        self.nimages = 0
 
     def process_event(self, event: EventData, parameters=None):
-        pass
+        sardana = None
+        if "sardana" in event.streams:
+            sardana = sardana_parse(event.streams["sardana"])
 
+        dat = None
+        if "orca" in event.streams:
+            dat = parse(event.streams["orca"])
+        if "andor3_balor" in event.streams:
+            dat = parse(event.streams["andor3_balor"])
+
+        if isinstance(dat, Stream1Start):
+            return {"filename": dat.filename}
+
+        elif isinstance(dat, Stream1Data):
+            pileup = parameters["pileup"].value
+            if pileup:
+                if self.pile is None:
+                    self.pile = np.zeros_like(dat.data, dtype=np.uint32)
+                self.pile += dat.data
+                self.nimages += 1
+        elif isinstance(dat, Stream1End):
+            pileup = parameters["pileup"].value
+            print("send partial pileup")
+            if pileup:
+                return {"pile": self.pile, "nimages": self.nimages}
 
     def finish(self, parameters=None):
         print("finished")
